af608b1c0d8e075504e12acac742eef955715aa3
Saitou <yugo030411@gmail.com>
2022-01-11 11:57:55 +0900

    typo

--- app.py
+++ app.py
@@ -13,29 +13,29 @@ from linebot.models import (
 # import sentencepiece as spm
 
 from SentensePiece import reply
-import os
-import requests
+# import os
+# import requests
 
 
 app = Flask(__name__)
 
-TALKAPI_KEY = os.environ['YOUR_API']
+# TALKAPI_KEY = os.environ['YOUR_API']
 app.config['CHANNEL_ACCESS_TOKEN'] = 'U/oVg3fG2HXcIMdcLWeNcGJBGkIWZkmFQKlu6RbsiIGS0/5Skf//k4NBTelHhGEZO7muRgTldBTMYGK+tBlpYiBMuPOXWFed3s2lpjod4wCNG5Q1w/lWsGIaNyNEWSz0JouYkBv+VsOnqHtW6IsP6gdB04t89/1O/w1cDnyilFU='
 app.config['CHANNEL_SECRET'] = '3105443c8351506641764c0ea3353fbd'
 
 line_bot_api = LineBotApi(app.config['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(app.config['CHANNEL_SECRET'])
 
-def talkapi(text):
-   url = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
-   req = requests.post(url, {'apikey':TALKAPI_KEY,'query':text}, timeout=5)
-   data = req.json()
+# def talkapi(text):
+#    url = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
+#    req = requests.post(url, {'apikey':TALKAPI_KEY,'query':text}, timeout=5)
+#    data = req.json()
 
-   if data['status'] != 0:
-      return data['message']
+#    if data['status'] != 0:
+#       return data['message']
 
-   msg = data['results'][0]['reply']
-   return msg
+#    msg = data['results'][0]['reply']
+#    return msg
 
 @app.route("/")
 def say_hello():
@@ -65,22 +65,22 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    push_text = event.message.text
-    msg = talkapi(push_text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=msg))
+    # push_text = event.message.text
+    # msg = talkapi(push_text)
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=msg))
     
     # ここで文章生成
-    # try:
-    #     reply_text = reply()
-    #     line_bot_api.reply_message(
-    #         event.reply_token,
-    #         TextSendMessage(text=reply_text))
-    # except BaseException:
-    #     line_bot_api.reply_message(
-    #         event.reply_token,
-    #         TextSendMessage(text="返信失敗..."))
+    try:
+        reply_text = reply()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_text))
+    except BaseException:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="返信失敗..."))
     # finally:
     #     print("aaa")
 
